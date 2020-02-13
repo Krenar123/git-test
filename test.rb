3681bace7df113644767643389dde8a1d6345588
Krenar123 <krenardardhishta205@gmail.com>
2020-02-13 20:10:27 +0100

    New file

--- test.rb
+++ test.rb
@@ -1,13 +1,23 @@
-<<<<<<< HEAD
 # Creating a ruby quiz game
 
 hash = Hash.new
-name  = gets.chomp
+ans = Hash.new
+q_num = gets.to_i
 id = 1
-
-hash[id] = name
-
-hash.each { |i| puts i[id]}
-=======
-puts "New file"
->>>>>>> Test
+q_num.times do
+    question = gets.chomp
+    hash[id] = question
+    answer = gets.chomp
+    ans[id] = answer
+    id += 1
+end
+result  = 0
+hash.each do
+    |key,value|
+    puts value + " a-Po , b-Jo"
+    given_ans = gets.chomp
+    if given_ans == ans[key]
+        result += 1
+    end
+end
+puts "#{result} / #{id-1}"
